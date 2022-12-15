4dc79c3c6f1f43d925e265980a3810548d317a29
xNexus-ACS <83370388+xNexus-ACS@users.noreply.github.com>
2022-12-16 00:41:36 +0100

    1.0.0

---
+++ FixTutorialPosition/MainClass.cs
@@ -0,0 +1,26 @@
+﻿using PlayerRoles;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+using UnityEngine;
+
+namespace FixTutorialPosition
+{
+    public class MainClass
+    {
+        [PluginEntryPoint("FixTutorialPosition", "1.0.0", "Teleport Tutorials to the tower again", "xNexusACS")]
+        public void Enable()
+        {
+            EventManager.RegisterEvents(this);
+        }
+
+        [PluginEvent(ServerEventType.PlayerChangeRole)]
+        public void OnPlayerChangeRole(Player player, PlayerRoleBase oldRole, RoleTypeId newRole,
+            RoleChangeReason changeReason)
+        {
+            if (newRole is RoleTypeId.Tutorial && oldRole.RoleTypeId is not RoleTypeId.Tutorial)
+                player.Position = new Vector3(40.297f, 1014.110f, -31.918f);
+        }
+    }
+}
